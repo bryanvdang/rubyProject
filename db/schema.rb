57616b25120995cb466cc12ed931383fc65da090
Bryan Dang <bryanvdang@gmail.com>
2016-12-27 23:56:57 -0700

    set User Permissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227104630) do
+ActiveRecord::Schema.define(version: 20161228053701) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20161227104630) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
